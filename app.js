f24d66ddb6c0fab30daf46b2b5bf916638b7760d
OdulNibas <47628891+OdulNibas@users.noreply.github.com>
2019-02-14 16:41:43 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+var content = document.getElementById('content');
+var truc = document.getElementById('truc');
+
+truc.addEventListener('click', function(){
+
+  content.style.transform = "rotate(1250deg)";
+  content.style.transition = "all 0.5s";
+  content.style.backgroundColor = "#aa1728";
+  content.style.borderRadius = "40%/15px";
+  truc.style.transform = "rotate(-180deg)";
+  truc.textContent = "euh j'ai la tête qui tourne là, buurp !!";
+});
